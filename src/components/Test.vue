d4669cda9fdebec56b93052383d889fcd56b8b8f
xna <xna00@qq.com>
2021-01-23 21:07:51 +0800

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ counter }}</div>
+  <button @click="increment">+1</button>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+const counter = ref(0);
+const increment = () => counter.value++;
+</script>
